8cdba5385f041eff8397a4e7f84677830c9af104
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-29 08:52:14 +0700

    arrays

---
+++ typescript-training/03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"]
+
+for (let tempSport of sportOne) {
+    if (tempSport == "Cricket") {
+        console.log(tempSport + " >> My Fav!!")
+    } else {
+        console.log(tempSport)
+    }
+}
